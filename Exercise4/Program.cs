9b146c8d684bca465f6137bbb6fb61f494f7514c
Vivekkumar260892 <vivekkumar1@mail.usf.edu>
2019-01-22 04:25:58 -0500

    Exercise_4

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace Exercise4
+namespace Exercise_4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Input any Number");
+            String Input = Console.ReadLine();
+            int a = Convert.ToInt32(Input);
+            int i, j;
+            for (i = 1; i <= a; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
